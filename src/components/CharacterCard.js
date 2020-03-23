3dc1c33de4c879a2496025aae80bd99902396b98
Kadeem140 <Beckford.kadeem@yahoo.com>
2020-03-22 20:51:10 -0400

    Finished Character Card, needs styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+// id: 19
+// name: "Antenna Rick"
+// status: "unknown"
+// species: "Human"
+// type: "Human with antennae"
+// gender: "Male"
+// origin: {name: "unknown", url: ""}
+// location: {name: "unknown", url: ""}
+export default function CharacterCard({character}) {
+    console.log('Character Card props', character)
+  return (
+      <div>
+          <h1>{character.name}</h1>
+            <p>{character.status}</p>
+            <p>{character.species}</p>
+            <p>{character.type}</p>
+            <p>{character.gender}</p>
+            <p>{character.origin.name}</p>
+            <p>{character.location.name}</p>
+      </div>
+
+  )
 }
